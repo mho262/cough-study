32b0f2f87bf8292aa5ff75acb9a71a152b2a67a8
Michelle Ho <ng.mi@outlook.com>
2024-11-02 23:18:28 -0400

    added assets

---
+++ assets/js/database.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBt0eK94gqW-82sgOGX60of1R2JuuPBINg",
+  authDomain: "cough-study.firebaseapp.com",
+  projectId: "cough-study",
+  storageBucket: "cough-study.firebasestorage.app",
+  messagingSenderId: "908273194323",
+  appId: "1:908273194323:web:62707ad32e384d0081bda6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
